ffc2fa638390186bea5f2dc53dfe6f595715a692
pleasworkwhyywont <micahp11585@mnpsk12.org>
2024-11-18 16:33:10 +0000

    rename

--- test-project/src/main.rs
+++ test-project/src/main.rs
@@ -1,25 +1,46 @@
+
+
 /*--------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-pub struct MultiUse<D : Copy> {
-    uses : usize,
-    data : Box<D>
-}
-impl<D : Copy> MultiUse<D> {
-    fn new(amout : usize, value: D ) -> Self {
-        MultiUse { uses: (amout), data: Box::new(value) }
-    }
-    fn open(&mut self) -> D {
-        if self.uses == 0 {
-            panic!("you have used this variable to many times. You must use the revive method/nin this MultiUse");}
-        self.uses -= 1;
-        *self.data.clone()
-    }
-    fn revive(&mut self,lives : usize) {
-        self.uses += lives
-    }
-    fn set(&mut self,data : D) {
+macro_rules! creat_MultiUse {
+    (set,$name:ident) => {
+    fn set(&mut self , data : D) {
         self.data = Box::new(data)
     }
+    };
+    (new,$name:ident) => {
+    fn new(amout : usize, value: D ) -> Self {
+        $name { uses: (amout), data: Box::new(value) }
+    }
+    };
+    (revive,$name:ident) => {
+    fn revive(&mut self,lives : usize) {
+            self.uses += lives
+    }
+    };
+    (open,$name:ident) => {
+        fn open(&mut self) -> D {
+            if self.uses == 0 {
+                panic!("you have ran out of the provided amout of uses")
+            }
+            self.uses -= 1;
+            **&self.data
+        }
+    };
+    ($name:ident _sep_$($methods:ident!),*) => {
+    pub struct $name<D : Copy> {
+        uses : usize,
+        data : Box<D>
+    }
+    impl<D : Copy> $name<D> {
+        $(creat_MultiUse!{$methods,$name})*
+    }
+
+    };
 }
+creat_MultiUse!{MutMultiUse _sep_ new!,revive!,set!,open!}
+creat_MultiUse!{ConMultiUse _sep_ new!,revive!,open!}
+creat_MultiUse!{NonRevMutMultiUse _sep_ new!,set!,open!}
+creat_MultiUse!{NonRevConMultiUse _sep_ new!,open!}
